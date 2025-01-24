936ad740fe52b841395c437d6751db12a5f6a89c
FrozenFrost6 <ch.anvesh2002@gmail.com>
2025-01-24 17:46:17 -0500

    Added connectivity from backend to front end. Updated expense variable names so that front end uses same names as backend. created an auth reducer to store login creds. created a global config file that contains all api paths.

--- src/components/ExpenseDashboard.js
+++ src/components/ExpenseDashboard.js
@@ -1,14 +1,39 @@
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import axios from "axios";
 import ExpenseList from "./ExpenseList";
 import ExpenseListFilters from "./ExpenseListFilters";
-
+import { addExpense } from "../slices/expensesSlice";
 
 function ExpenseDashboard() {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        const fetchExpenses = async () => {
+            try {
+                const response = await axios.get('http://localhost:8080/expenses', {
+                    auth: {
+                        username: 'anveshch',
+                        password: 'pass'
+                    }
+                });
+                response.data.forEach(expense => {
+                    dispatch(addExpense(expense));
+                });
+            } catch (error) {
+                console.error("There was an error fetching the expenses!", error);
+            }
+        };
+
+        fetchExpenses();
+    }, [dispatch]);
+
     return ( 
         <div>
             <ExpenseListFilters />
             <ExpenseList />
         </div>
-     );
+    );
 }
 
 export default ExpenseDashboard;
